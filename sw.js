1a6c25fce2a520444dec2a6cc7f77bd398528601
Dataidoo <sbbrampton@gmail.com>
2026-06-10 23:02:10 -0400

    o

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'dash-v1';
+const SHELL = ['./dashboard.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).catch(() => caches.match('./dashboard.html'))
+    )
+  );
+});
